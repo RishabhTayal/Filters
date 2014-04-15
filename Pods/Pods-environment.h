f5b520c8f294fe850f3f9cfb2aa73e0cd3c98451
Rishabh Tayal <rtayal11@gmail.com>
2014-04-15 13:51:12 -0500

    pod.

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -48,6 +48,12 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 2
 #define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 1
 
+// GoogleAnalytics-iOS-SDK
+#define COCOAPODS_POD_AVAILABLE_GoogleAnalytics_iOS_SDK
+#define COCOAPODS_VERSION_MAJOR_GoogleAnalytics_iOS_SDK 3
+#define COCOAPODS_VERSION_MINOR_GoogleAnalytics_iOS_SDK 0
+#define COCOAPODS_VERSION_PATCH_GoogleAnalytics_iOS_SDK 6
+
 // SwipeView
 #define COCOAPODS_POD_AVAILABLE_SwipeView
 #define COCOAPODS_VERSION_MAJOR_SwipeView 1
